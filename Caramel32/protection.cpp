2ecfc800f55b06107ad89175c02e1c17b172da6c
GShabanov <51275640+GShabanov@users.noreply.github.com>
2021-03-12 15:26:15 +0200

    Protection DLL, initial commit

---
+++ Caramel32/protection.cpp
@@ -0,0 +1,16 @@
+/***************************************************************************************\
+*   File:                                                                              *
+*       protection.h                                                                   *
+*                                                                                      *
+*   Abstract:                                                                          *
+*       protection hook module                                                         *
+*                                                                                      *
+*   Author:                                                                            *
+*       G Shabanov          11-Mar-2021                                                *
+*                                                                                      *
+*   Revision History:                                                                  *
+/***************************************************************************************/
+// $Id: $
+
+#include "stdafx.h"
+#include "protection.h"
---
+++ Caramel32/protection.h
@@ -0,0 +1,20 @@
+/***************************************************************************************\
+*   File:                                                                              *
+*       protection.h                                                                   *
+*                                                                                      *
+*   Abstract:                                                                          *
+*       protection hook module                                                         *
+*                                                                                      *
+*   Author:                                                                            *
+*       G Shabanov          11-Mar-2021                                                *
+*                                                                                      *
+*   Revision History:                                                                  *
+/***************************************************************************************/
+// $Id: $
+#pragma once
+#ifndef __PROTECTION_H__
+#define __PROTECTION_H__
+
+
+
+#endif // __PROTECTION_H__
